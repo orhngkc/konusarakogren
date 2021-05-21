509dad90a8c3a9a09d812775d15140fc88e96770
orhan.gokce@turkinvest.com <orhan.gokce@turkinvest.com>
2021-05-22 00:40:48 +0300

    hero, about, works

---
+++ assets/js/nav.js
@@ -0,0 +1,23 @@
+const navItems = document.querySelectorAll('.nav-item');
+const active = 'active';
+
+navItems.forEach(element => {
+    element.addEventListener('click', (e) => {
+        navItems.forEach(element => {element.classList.remove(active)})
+        element.classList.add(active)
+    })
+});
+
+
+
+
+
+const worksCategory = document.querySelectorAll('.works-category');
+const textPrimary = 'text-primary';
+
+worksCategory.forEach(element => {
+    element.addEventListener('click', (e) => {
+        worksCategory.forEach(element => {element.classList.remove(textPrimary)})
+        element.classList.add(textPrimary)
+    })
+});
